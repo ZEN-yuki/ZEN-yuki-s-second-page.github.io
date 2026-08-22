d036281c170a7ca26035a6629d9af166353de403
ZEN-yuki <251zs01932@student.zen.ac.jp>
2026-08-23 02:48:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const menus = [
+    "カレー",
+    "オムライス",
+    "ハンバーグ",
+    "パスタ",
+    "親子丼"
+];
 
+const pushButtom = document.getElementbyid("pushButtom");
+pushButtom.addEventListener("click",function(){
+  console.log("ボタンが押された");
+  )};
