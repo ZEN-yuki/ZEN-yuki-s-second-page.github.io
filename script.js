b4add096e0ff5ec7ad0f2e2b8350be05763327e5
ZEN-yuki <251zs01932@student.zen.ac.jp>
2026-08-23 03:10:25 +0900

    Fix syntax error in script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const pushButtom = document.getElementById("pushButtom");
 pushButtom.addEventListener("click", function() {
   console.log("ボタンが押された");
 });
-Math.random();
+Math.random()
 console.log(Math.random());
 Math.floor(Math.random() * menus.length)
 alert("JavaScriptが動いています");
