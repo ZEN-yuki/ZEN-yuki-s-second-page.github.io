ecf6319f695b6466540eb8e9383a9564c3ceb79e
ZEN-yuki <251zs01932@student.zen.ac.jp>
2026-08-23 03:09:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const menus = [
     "親子丼"
 ];
 
-const pushButtom = document.getElementbyid("pushButtom");
-pushButtom.addEventListener("click",function(){
+const pushButtom = document.getElementById("pushButtom");
+pushButtom.addEventListener("click", function() {
   console.log("ボタンが押された");
-  )};
+});
 Math.random();
 console.log(Math.random());
 Math.floor(Math.random() * menus.length)
