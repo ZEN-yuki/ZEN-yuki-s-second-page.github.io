3bc04975d54116af3ce5154603a68f88b33736c4
ZEN-yuki <251zs01932@student.zen.ac.jp>
2026-08-23 03:54:18 +0900

    Fix button click event to update result text

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const pushButtom = document.getElementById("pushButtom");
 const resulttext = document.getElementById("resulttext");
 pushButtom.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 10);
-    console.log("ボタンが押された");   
-});
-const selectmenu = menus[randomNumber];
+    console.log("ボタンが押された");  
+    
+    const selectmenu = menus[randomNumber];
 resulttext.textContent =selectmenu;
+
+});
